6c3cb4d98e69bf53c9c7cea4a1364ca009554f1c
hugosep <hugosep1@gmail.com>
2024-10-31 17:52:40 -0300

    init2

--- app/(dashboard)/user.tsx
+++ app/(dashboard)/user.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { auth, signOut } from 'auth';
+import { auth, signIn, signOut } from 'auth';
 import Image from 'next/image';
 import {
   DropdownMenu,
